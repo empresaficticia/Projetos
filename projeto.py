85a01ea950b062edceb47fefe74ba3800c627724
empresaficticia <156560925+empresaficticia@users.noreply.github.com>
2025-03-22 20:52:57 +0000

    Projeto
    Meu projeto secreto

---
+++ projeto.py
@@ -0,0 +1,57 @@
+import base64 #line:1:import base64
+import math #line:2:import math
+import random #line:3:import random
+import time #line:4:import time
+def do_1 ():
+    pass 
+def do_2 ():
+    pass 
+def do_3 ():
+    pass 
+def do_4 ():
+    pass 
+a =1 #line:18:a = 1
+b =2 #line:19:b = 2
+c =3 #line:20:c = 3
+d =4 #line:21:d = 4
+e =5 #line:22:e = 5
+f =6 #line:23:f = 6
+g =7 #line:24:g = 7
+h =8 #line:25:h = 8
+i =9 #line:26:i = 9
+j =10 #line:27:j = 10
+k =11 #line:28:k = 11
+l =12 #line:29:l = 12
+m =13 #line:30:m = 13
+n =14 #line:31:n = 14
+o =15 #line:32:o = 15
+p =16 #line:33:p = 16
+q =17 #line:34:q = 17
+r =18 #line:35:r = 18
+s =19 #line:36:s = 19
+t =20 #line:37:t = 20
+u =21 #line:38:u = 21
+v =22 #line:39:v = 22
+w =23 #line:40:w = 23
+x =24 #line:41:x = 24
+y =25 #line:42:y = 25
+z =26 #line:43:z = 26
+def adasdaslgppgfdjdfjldkafjsdlasfoljhjosfaslfoasjfjas ():#line:45:def random_func():
+    O0O0O0O0000OO00O0 =random .randint (1 ,100 )#line:46:a = random.randint(1, 100)
+    O0O0000OOO0O0O0OO =random .random ()#line:47:b = random.random()
+    return O0O0O0O0000OO00O0 +O0O0000OOO0O0O0OO #line:48:return a + b
+for i in range (5 ):#line:50:for i in range(5):
+    do_1 ()
+    do_2 ()
+time .sleep (0.5 )#line:54:time.sleep(0.5)
+if adasdaslgppgfdjdfjldkafjsdlasfoljhjosfaslfoasjfjas ()>50 :#line:56:if random_func() > 50:
+    do_3 ()
+time .sleep (1 )#line:59:time.sleep(1)
+def asdasd0safkçsafsafasfsajkfasjfasfasfk12f12jf21 ():#line:61:
+    while True :#line:62:while True:
+        break #line:63:break
+asdasd0safkçsafsafasfsajkfasjfasfasfk12f12jf21 ()#line:65
+obf =lambda OO000OOO00O00O00O :"".join (chr (O000O0OO000OO0OOO )for O000O0OO000OO0OOO in OO000OOO00O00O00O )#line:67:obf = lambda x: "".join(chr(c) for c in x)
+asdasdQ2RQfASfadsfsdfdsfdsfdsfdsfdsf2131 ="W0ZMQUcwNV1fUHl0aG9u"
+apasd1248129321893dsfsdfdsjfdskff2384324324jksdfse =base64 .b64decode (asdasdQ2RQfASfadsfsdfdsfdsfdsfdsfdsf2131 )
+print (apasd1248129321893dsfsdfdsjfdskff2384324324jksdfse .decode ('utf-8'))
